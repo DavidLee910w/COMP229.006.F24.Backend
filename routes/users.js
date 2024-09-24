6334f4575c67b047e2f2ae873d8d14a86ca5c9e9
DKCL1989 <devilee1989@gmail.com>
2024-09-23 22:13:17 -0400

    Express

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Hello Users');
 });
 
+/* GET users listing. */
+router.get('/David', function(req, res, next) {
+  res.send('Hello David');
+});
 module.exports = router;
